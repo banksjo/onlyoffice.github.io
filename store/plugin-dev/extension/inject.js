e694edc9159fa1ee00e0acad05c8a956cf7b2232
banksjo <jdbanks001@gmail.com>
2026-09-05 21:40:06 +1000

    Point extension to Coastal Letter Formatter

--- store/plugin-dev/extension/inject.js
+++ store/plugin-dev/extension/inject.js
@@ -1,5 +1,5 @@
 (function(window, undefined) {
-	var URL_TO_PLUGIN = "";
+    var URL_TO_PLUGIN = "https://banksjo.github.io/coastal-letter-formatter/";
 	
 	var xhrObj = new XMLHttpRequest();
 	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", false);
