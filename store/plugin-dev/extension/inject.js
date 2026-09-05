bbba29ff148ca3188c05c4413a0012a3d10b4241
banksjo <jdbanks001@gmail.com>
2026-09-05 23:33:11 +1000

    Modify config.json request to include versioning
    Updated XMLHttpRequest to include versioning in the config.json request to prevent caching issues.

--- store/plugin-dev/extension/inject.js
+++ store/plugin-dev/extension/inject.js
@@ -1,19 +1,62 @@
 (function(window, undefined) {
-    var URL_TO_PLUGIN = "https://banksjo.github.io/coastal-letter-formatter/";
-	
-	var xhrObj = new XMLHttpRequest();
-	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", false);
-	xhrObj.send('');
 
-	var configObj = JSON.parse(xhrObj.responseText);
-	configObj.baseUrl = URL_TO_PLUGIN;
+    var URL_TO_PLUGIN =
+        "https://banksjo.github.io/coastal-letter-formatter/";
 
-	window.Asc = window.Asc ? window.Asc : {};
-	window.Asc.extensionPlugins = window.Asc.extensionPlugins ? window.Asc.extensionPlugins : [];
-	window.Asc.extensionPlugins.push(configObj);
-	if (window.Asc.g_asc_plugins && window.Asc.g_asc_plugins.loadExtensionPlugins)
-	{
-		window.Asc.g_asc_plugins.loadExtensionPlugins(window.Asc.extensionPlugins);
-		window.Asc.extensionPlugins = [];
-	}	
+    /*
+     * v14:
+     * Force a fresh config.json request so ONLYOFFICE does not
+     * continue using an older cached plugin configuration.
+     */
+
+    var xhrObj =
+        new XMLHttpRequest();
+
+    xhrObj.open(
+        "GET",
+        URL_TO_PLUGIN + "config.json?v=14",
+        false
+    );
+
+    xhrObj.send("");
+
+
+    var configObj =
+        JSON.parse(
+            xhrObj.responseText
+        );
+
+
+    configObj.baseUrl =
+        URL_TO_PLUGIN;
+
+
+    window.Asc =
+        window.Asc || {};
+
+
+    window.Asc.extensionPlugins =
+        window.Asc.extensionPlugins
+            ? window.Asc.extensionPlugins
+            : [];
+
+
+    window.Asc.extensionPlugins.push(
+        configObj
+    );
+
+
+    if (
+        window.Asc.g_asc_plugins &&
+        window.Asc.g_asc_plugins.loadExtensionPlugins
+    ) {
+
+        window.Asc.g_asc_plugins
+            .loadExtensionPlugins(
+                window.Asc.extensionPlugins
+            );
+
+        window.Asc.extensionPlugins = [];
+    }
+
 })(window, undefined);
